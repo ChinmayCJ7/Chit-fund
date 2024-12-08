cf8efdbff1f5be84e05e6e8431dd6ed6e9eae518
Chinmay J <105866697+ChinmayCJ7@users.noreply.github.com>
2024-12-08 11:09:01 +0530

    3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="mt-auto bg-slate-100 w-screen text-center mt-16 bottom-0">
       <div className="p-2 text-center text-gray-600">
-        Team WAGBI
+        Team 
       </div>
     </footer>
   )
